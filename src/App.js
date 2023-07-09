55059a53fe712c841e68769ba2301e722b74be71
Rohit Sai Chowdary Potluri <89537171+rohit-sai-chowdary@users.noreply.github.com>
2023-07-09 08:35:24 -0400

    Notepad Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import NotesList from './components/NotesList'
+import { useEffect, useState } from 'react'
+import { nanoid } from 'nanoid'
+import Search from './components/Search'
+import Header from './components/Header'
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem('react-notes-app-data'))
+  )
+
+  const [searchText, setSearchText] = useState('')
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('react-notes-app-data', JSON.stringify(notes))
+  }, [notes])
+
+  const addNote = (text) => {
+    const date = new Date()
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    }
+    const newNotes = [...notes, newNote]
+    setNotes(newNotes)
+  }
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id)
+    setNotes(newNotes)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
